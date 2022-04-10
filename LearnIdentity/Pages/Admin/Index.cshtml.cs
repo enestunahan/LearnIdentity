36dd4dfb2e82149a6b8225da52661184a130974a
Enes Tunahan <enesstunahan@gmail.com>
2022-04-10 17:08:45 +0300

    sayfa yetkilendirme

--- LearnIdentity/Pages/Admin/Index.cshtml.cs
+++ LearnIdentity/Pages/Admin/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using LearnIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace LearnIdentity.Pages.Admin
 {
+    [Authorize(Roles ="admin")]
+    //[Authorize(Roles ="admin,yönetici")] birden fazla da rol ekleyebiliriz bu þekilde
     public class IndexModel : BasePageModelModel
     {
         public IndexModel() : base(null,null,null)
